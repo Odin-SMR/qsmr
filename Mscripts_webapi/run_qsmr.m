4bfc684fad42a6c77b9ea9b02069ab9a1549ba95
Andreas Skyman <andreas.skyman@molflow.com>
2016-05-27 15:59:26 +0200

    Added simpler function for running QSMR
    Added function "run_qsmr.m" to Mscripts_webapi for simpler invocation of
    the inversion. run_qsmr(URI) takes a URI to a scan log as an argument
    and otherwise replicates the functionality from the old invocation in
    the docker-compose file.

---
+++ Mscripts_webapi/run_qsmr.m
@@ -0,0 +1,5 @@
+function [L2, L2I] = run_qsmr(URI)
+    LOG = get_scan_log(URI);
+    fm = LOG.Info.FreqMode;
+    L1B = get_scan_l1b_data(LOG.Info.URLS.URL_spectra);
+    [L2, L2I] = q2_inv(LOG.Info, L1B, q_std(fm))
