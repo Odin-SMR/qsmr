da094c8fe3b8b558d67cf916110ee85364ebd401
Jimmy Petersson <jimmy@molflow.com>
2016-08-29 15:04:17 +0200

    Add support for data delivery to api + fixes
    Summary:
    Reset LD_LIBRARY_PATH when calling arts
    
    Add optional target url argument
    
    Add support for target api auth
    
    Break build if failure
    
    Fix syntax
    
    Test Plan: Test by running script via docker repo
    
    Reviewers: joakim
    
    Reviewed By: joakim
    
    Differential Revision: http://phabricator.molflow.com/D42

--- runscript.m
+++ runscript.m
@@ -1,11 +1,14 @@
-% example url: http://malachite.rss.chalmers.se/rest_api/v4/freqmode_info/2015-04-01/AC2/1/7123991206/
-function []=runscript(url)
+% Run calculation and send results as json to an api if target_url
+% is provided. If not, write results to .mat files.
+% example source url:
+% http://malachite.rss.chalmers.se/rest_api/v4/freqmode_info/2015-04-01/AC2/1/7123991206/
+function []=runscript(source_url, target_url, target_username, target_password)
    %run('Mscripts_qsystem/q2_init.m');
-   LOG = get_scan_log(url)
+   LOG = get_scan_log(source_url)
    L1B = get_scan_l1b_data(LOG.Info.URLS.URL_spectra)
 
    % Extract freq mode from url
-   urlparts = strsplit(url, '/')
+   urlparts = strsplit(source_url, '/')
    if isequal(urlparts(end), {''})
       freqmode = cellfun(@str2num, urlparts(end-2))
    else
@@ -13,6 +16,14 @@ function []=runscript(url)
    end
 
    [L2,L2I] = q2_inv( LOG.Info, L1B, q_docker(freqmode))
-   save('l2.mat', '-ascii', 'L2')
-   save('l2i.mat', '-ascii', 'L2I')
+
+   if nargin < 2
+      save('L2.mat', 'L2')
+      save('L2I.mat', 'L2I')
+   else
+       options = weboptions('MediaType','application/json', 'Username', ...
+                            target_username, 'Password', target_password)
+       data = struct('L2', L2, 'L2I', L2I)
+       response = webwrite(target_url, data, options)
+   end
 return
