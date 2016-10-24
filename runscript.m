4bc3653cad4185dc60a55fa786ccd13dc0342775
Jimmy Petersson <jimmy@molflow.com>
2016-10-24 07:54:54 +0200

    runscript.m: Adapt to L2C

--- runscript.m
+++ runscript.m
@@ -7,11 +7,12 @@ function []=runscript(source_url, target_url, target_username, target_password)
    LOG = get_scan_log(source_url)
    L1B = get_scan_l1b_data(LOG.Info.URLS.URL_spectra)
 
-   [L2,L2I] = q2_inv( LOG.Info, L1B, q_docker(LOG.Info.FreqMode))
+   [L2,L2I,L2C] = q2_inv( LOG.Info, L1B, q_docker(LOG.Info.FreqMode))
 
    if nargin < 2
       save('L2.mat', 'L2')
       save('L2I.mat', 'L2I')
+      save('L2C.mat', 'L2C')
    else
        if nargin < 3
            options = weboptions('MediaType','application/json')
@@ -20,9 +21,8 @@ function []=runscript(source_url, target_url, target_username, target_password)
                                 target_username, 'Password', ...
                                 target_password)
        end
-       data = struct('L2', L2, 'L2I', L2I)
+       data = struct('L2', L2, 'L2I', L2I, 'L2C': L2C)
        response = webwrite(target_url, data, options)
-       % TODO: Exit with failure if webwrite failed
    end
    fclose('all')
 exit(0)
