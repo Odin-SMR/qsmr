49fecbc5069b484432de54e48d425985d5c55905
Jimmy Petersson <jimmy@molflow.com>
2016-11-16 10:10:18 +0100

    Use env in data artifact when building final image

--- Build_scripts/docker/build_complete.sh
+++ Build_scripts/docker/build_complete.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-# TODO:
-# invemode and freqmode as parameters
-# create data artifact with precalc docker image
-# rm old complete/data and copy artifact files to complete/data
-# build
+source complete/data/env.sh
 
 today=$(date +%y%m%d)
 
-docker build -t "docker2.molflow.com/devops/qsmr_stnd_1:${today}" complete/
-#docker push "docker2.molflow.com/devops/qsmr_stnd_1:${today}"
+image_name="qsmr_${INVEMODE}_${FREQMODE}"
+
+echo $image_name
+
+docker build -t "docker2.molflow.com/devops/${image_name}:${today}" complete/
+#docker push "docker2.molflow.com/devops/${image_name}:${today}"
